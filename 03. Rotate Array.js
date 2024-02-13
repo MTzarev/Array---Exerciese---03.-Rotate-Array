2ba407262835637dbf5e56ce3254278db2168612
Miroslav Tzarev <mtzarev@gmail.com>
2024-02-13 22:13:00 +0200

    Add files via upload

---
+++ 03. Rotate Array.js
@@ -0,0 +1,17 @@
+function rotateArray(arr) {
+    let a = arr.pop()
+    let length = arr.length
+    let newArr = [];
+    let rotateArr = []
+
+    for (let i = 0; i < arr.length; i++) {
+newArr+=arr[i]
+                  }
+    for (let i=0; i<=a%arr.length; i++){
+        
+    }
+  
+    console.log(newArr);
+}
+rotateArray(['1', '2', '3', '4', '2']);
+//rotateArray(['Banana', 'Orange', 'Coconut', 'Apple', '15']);
